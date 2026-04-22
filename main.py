f40d4d60883f22a87f9da46f125731022a223658
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-04-21 22:41:32 -0300

    Implementacao do SP Retrieval com Cypher do Neo4j

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from Dataset.dataset import load_graph
 from Processamento.avaliacao import Evaluator
 from Processamento.extrair import extract_entity
 from Dataset.dataset import load_qa
-
-
+import Semantic_Parsing.neo4SPretrival as neoSP
 # carregar grafo
 G = load_graph("Benchmark/kb.txt")
 
@@ -15,14 +14,37 @@ qa_data = load_qa("Benchmark/1-hop/vanilla/qa_train.txt")
 evaluator = Evaluator()
 k = 5
 
+# # loop principal
+# for q, a in qa_data:
+    
+#     #1 - Extrair entidade da pergunta
+#     entity = extract_entity(q)
+    
+#     #2 - Calcula candidatos a resposta
+#     candidates = bfs_answer(G, entity)
+    
+#     #3 - Armazena resposta final
+#     prediction = candidates[:k] if candidates else []
+    
+#     #4 - Compara resposta encontrada com a resposta certa 
+#     evaluator.update(prediction, a)
+
+# # resultado final
+# evaluator.report()
+
 # loop principal
 for q, a in qa_data:
     
     #1 - Extrair entidade da pergunta
     entity = extract_entity(q)
+    relation = neoSP.parse_question(q)
     
+    if relation is None:
+        evaluator.update([], a)
+        continue
+
     #2 - Calcula candidatos a resposta
-    candidates = bfs_answer(G, entity)
+    candidates = neoSP.sp_query(G, entity, relation)
     
     #3 - Armazena resposta final
     prediction = candidates[:k] if candidates else []
